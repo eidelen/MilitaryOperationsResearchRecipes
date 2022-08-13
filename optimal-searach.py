3a506da91f42abad0345f41bc96762197ea9d667
Adrian Schneider <adrian.schneider.pr@gmail.com>
2022-08-13 21:58:08 +0200

    trying around with solvers

--- optimal-searach.py
+++ optimal-searach.py
@@ -2,6 +2,7 @@ import math
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+import scipy.optimize as opt
 
 def doOptSearch():
     print("Optimal Search Strategy - armasuisse W+T")
@@ -20,6 +21,22 @@ def doOptSearch():
         print(area_name + ": " + area_type + ", " + str(area_size) + " m^2, " + "p=" + str(prop))
 
 
+    my_constraints = ({'type': 'eq', "fun": apply_sum_constraint})
+
+    fun = lambda ti: minimizeFunctionProbDetection(areas, w, v, ti)
+    res_cons = opt.minimize(fun, (t/6, t/6, t/6, t/6, t/6, t/6),
+                                 method='SLSQP', options={'disp': True, 'eps': 1}, constraints=my_constraints)
+
+    print(res_cons)
+
+
+    # use scipy optimizer
+
+    #bnds = ((0, t), (0, t), (0, t), (0, t), (0, t), (0, t))
+    #res = opt.minimize(fun, (t/6, t/6, t/6, t/6, t/6,t/6), method='TNC', bounds=bnds, tol=1e-10, options={'disp': True})
+
+    #print(res)
+
 
     #x_t = np.arange(0., 21600, 1)
     #y_t = np.array([randomSearchKoopman(w, v, 16000000, x) for x in x_t])
@@ -29,17 +46,34 @@ def doOptSearch():
 
 
 
-    maxProb = -1.0;
-    maxEffortConf = []
+    minProb = 1.0;
+    minEffort = []
 
-    for x in range(10000000):
+    for x in range(1000000):
         rndEfforts = np.random.rand(nAreas)
         rndEfforts = t / np.sum(rndEfforts) * rndEfforts
-        thisRun = computeOverallProbabilityOfDetection(areas, w, v, rndEfforts)
-        if thisRun > maxProb:
-            maxProb = thisRun
-            maxEffortConf = rndEfforts
-            print(x, ":", maxProb, maxEffortConf)
+        thisRun = minimizeFunctionProbDetection(areas, w, v, rndEfforts)
+        if thisRun < minProb:
+            minProb = thisRun
+            minEffort = rndEfforts
+            print(x, ":", minProb, minEffort)
+
+def apply_sum_constraint(inputs):
+    # return value must come back as 0 to be accepted
+    # if return value is anything other than 0 it's rejected
+    # as not a valid answer.
+    total = 5 * 60 * 60 - np.sum(inputs)
+    return total
+
+def minimizeFunctionProbDetection(allAreas, sweepWidth, velocity, efforts):
+    # transform maximize to minimize problem
+    optSum = 0
+
+    for ar, ti in zip(allAreas, efforts):
+        areaName, areaType, areaSize, areaProp = ar
+        optSum = optSum + areaProp * (1.0 - randomSearchKoopman(sweepWidth, velocity, areaSize, ti))
+
+    return optSum
 
 
 def computeOverallProbabilityOfDetection(allAreas, sweepWidth, velocity, efforts):
@@ -57,14 +91,7 @@ def meanTimeOfDetection(sweepWidth: float, velocity: float, area: float):
 
 
 def randomSearchKoopman(sweepWidth: float, velocity: float, area: float, time: float):
-    '''
-    The probability of detecting a target in given time.
-    :param sweepWidth:
-    :param velocity:
-    :param area:
-    :param time:
-    :return:
-    '''
+    # The probability of detecting a target in given time.
     return 1.0 - math.exp(-time/meanTimeOfDetection(sweepWidth, velocity, area))
 
 if __name__ == "__main__":
