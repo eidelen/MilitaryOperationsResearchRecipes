f079584b177a59336a64135f84a8e7477b394c86
Adrian Schneider <adrian.schneider.pr@gmail.com>
2022-08-14 14:30:56 +0200

    solve by trial

--- optimal-searach.py
+++ optimal-searach.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+import itertools
 import random
 import matplotlib.pyplot as plt
 import scipy.optimize as opt
@@ -11,23 +12,23 @@ def doOptSearch():
     w = 200  # m
     v = 10   # m/s
     t = 5 * 60 * 60 # s
-    areas = [("A1", "urban", 14100000, 0.6), ("A2", "mountain", 6300000, 0.03), ("A3", "mountain", 4100000, 0.03),
-             ("A4", "water", 3500000, 0.15), ("A5", "water", 1900000, 0.15), ("A6", "mountain", 9100000, 0.03)]
-
-    nAreas = len(areas)
+    areas = [("A1", "urban", 14100000, 0.40), ("A2", "mountain", 6300000, 0.10), ("A3", "mountain", 4100000, 0.10),
+             ("A4", "water", 3500000, 0.15), ("A5", "water", 1900000, 0.15), ("A6", "mountain", 9100000, 0.10)]
 
     print("w=" + str(w) + " m, v=" + str(v) + " m/s, t=" + str(t) + " s")
     for area_name, area_type, area_size, prop in areas:
         print(area_name + ": " + area_type + ", " + str(area_size) + " m^2, " + "p=" + str(prop))
 
+    solveByTrying(areas, w, v, t)
 
-    my_constraints = ({'type': 'eq', "fun": apply_sum_constraint})
 
-    fun = lambda ti: minimizeFunctionProbDetection(areas, w, v, ti)
-    res_cons = opt.minimize(fun, (t/6, t/6, t/6, t/6, t/6, t/6),
-                                 method='SLSQP', options={'disp': True, 'eps': 1}, constraints=my_constraints)
+    #my_constraints = ({'type': 'eq', "fun": apply_sum_constraint})
 
-    print(res_cons)
+    #fun = lambda ti: minimizeFunctionProbDetection(areas, w, v, ti)
+    #res_cons = opt.minimize(fun, (t/6, t/6, t/6, t/6, t/6, t/6),
+    #                             method='SLSQP', options={'disp': True, 'eps': 1}, constraints=my_constraints)
+
+    # print(res_cons)
 
 
     # use scipy optimizer
@@ -46,17 +47,61 @@ def doOptSearch():
 
 
 
-    minProb = 1.0;
-    minEffort = []
+    # minProb = 1.0;
+    # minEffort = []
+    #
+    # for x in range(1000000):
+    #     rndEfforts = np.random.rand(nAreas)
+    #     rndEfforts = t / np.sum(rndEfforts) * rndEfforts
+    #     thisRun = minimizeFunctionProbDetection(areas, w, v, rndEfforts)
+    #     if thisRun < minProb:
+    #         minProb = thisRun
+    #         minEffort = rndEfforts
+    #         print(x, ":", minProb, minEffort)
 
-    for x in range(1000000):
-        rndEfforts = np.random.rand(nAreas)
-        rndEfforts = t / np.sum(rndEfforts) * rndEfforts
-        thisRun = minimizeFunctionProbDetection(areas, w, v, rndEfforts)
-        if thisRun < minProb:
-            minProb = thisRun
-            minEffort = rndEfforts
-            print(x, ":", minProb, minEffort)
+def solveByTrying(allAreas, sweepWidth, velocity, totalTime):
+    nAreas = len(allAreas)
+
+    # generate trials
+    x = np.linspace(-1.0, 1.0, num=10)
+    trls = [trls for trls in itertools.product(x, repeat=nAreas)]
+    nTrials = len(trls)
+    iTri = 0
+    maxProb = 0
+    maxEfforts = []
+    amplifiers = np.full(nAreas, totalTime/2.0) # all areas are tried with full effort scale
+    bases = np.full(nAreas, totalTime/2.0)
+    for solveIters in range(5):
+
+        amplifiers = amplifiers / (solveIters+1)
+
+        print("amp:", amplifiers)
+        print("base:", bases)
+
+        for tr in trls:
+            # adjust to total invested effort
+            iTri = iTri + 1
+            nextEfforts = (np.array(tr) * amplifiers) + bases
+            nextEfforts = np.clip(nextEfforts, 0.0, totalTime)
+            sumNextEfforts = np.sum(nextEfforts)
+            if sumNextEfforts > 0.0001:
+                nextEfforts = nextEfforts * totalTime / np.sum(nextEfforts)
+                detectProb = computeOverallProbabilityOfDetection(allAreas, sweepWidth, velocity, nextEfforts)
+                if detectProb > maxProb:
+                    maxProb = detectProb
+                    maxEfforts = nextEfforts
+                    print( iTri, "(", iTri/nTrials*100, ")", ": New best trial: ", maxProb, maxEfforts)
+
+        # adjust base to best solution
+        bases = maxEfforts
+
+    print("Best Result: ", maxProb, maxEfforts)
+
+
+
+
+
+
 
 def apply_sum_constraint(inputs):
     # return value must come back as 0 to be accepted
