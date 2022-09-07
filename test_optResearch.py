349fc2882948af1a47863d7990a1d4f57e530323
Adrian Schneider <adrian.schneider.pr@gmail.com>
2022-09-07 21:42:09 +0200

    Test pipeline

---
+++ test_optResearch.py
@@ -0,0 +1,17 @@
+import unittest
+import localization
+import math
+import numpy as np
+
+class TestOperationsReseachRecipes(unittest.TestCase):
+
+    def test_localization_distance(self):
+        # line: y = x
+        # test points on line -> zero dist
+        for i in np.linspace(-10.0, 10.0, num=200):
+            dist = localization.dist_pnt2line((math.pi / 4, (0.0, 0.0)), (i, i) )
+            self.assertAlmostEqual(dist, 0.0)
+
+
+if __name__ == '__main__':
+    unittest.main()
