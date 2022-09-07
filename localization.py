723ab2c78b91beff7b48d0cf1d4433223bb7c30e
Adrian Schneider <adrian.schneider.pr@gmail.com>
2022-09-07 21:21:03 +0200

    finished main impl

--- localization.py
+++ localization.py
@@ -1,48 +1,28 @@
 import math
+import scipy.optimize as opt
 
-
-def comp_a(sensors):
-    a = 0
-    for ang, _, sensorerr in sensors:
-        a += math.sin(ang)**2 / sensorerr**2
-    return a
-
-def comp_b(sensors):
-    b = 0
-    for ang, _, sensorerr in sensors:
-        b += math.cos(ang)**2 / sensorerr**2
-    return b
-
-def comp_h(sensors):
-    h = 0
-    for ang, _, sensorerr in sensors:
-        h += math.sin(ang)*math.cos(ang) / sensorerr**2
-    return -h
-
-def comp_f(sensors):
-    f = 0
-    for ang, linedist, sensorerr in sensors:
-        f += linedist*math.sin(ang) / sensorerr**2
-    return -f
-
-def comp_g(sensors):
-    g = 0
-    for ang, linedist, sensorerr in sensors:
-        g += linedist*math.cos(ang) / sensorerr**2
-    return g
-
-def dist_pnt2line( line, point ):
+def dist_pnt2line(line, point):
+    '''
+    Shortest distance between a point a line.
+    :param line: a line (angle, point on line)
+    :param point: a point (x,y)
+    :return: minimal distance
+    '''
     lineAngle, pntOnLine = line
     pntLineX, pntLineY = pntOnLine
     pntX, pntY = point
     return math.cos(lineAngle)*(pntLineY-pntY) - math.sin(lineAngle)*(pntLineX - pntX)
 
-
-def accum_distSQ(lines, point):
+def sensor_errors_accum(sensors, point):
+    '''
+    Accumulates the localization error over all sensors.
+    :param sensors: all sensors, each having a line and a localization accuracy
+    :param point: target location
+    :return: accumulated target location error
+    '''
     sqDist = 0
-    x, y = point
-    for lineAng, linePnt, lineAccuracy in lines:
-        sqDist += dist_pnt2line((lineAng, linePnt), point) ** 2
+    for line, sensorAccuracy in sensors:
+        sqDist += dist_pnt2line(line, point) ** 2
     return sqDist
 
 
@@ -50,11 +30,12 @@ if __name__ == "__main__":
     print("Multi Sensor Localization - armasuisse W+T, Adrian Schneider")
     print("############################################################\n")
 
-    # (line angle, point on line, sensor accuracy (as lower as better))
-    sensors = [(0.0, (0.0, 0.0), 1.0),    # y = 0x + 0
-               (math.pi / 4, (0.0, 0.0), 1.0)] # y = 1x + 0
+    # ((line definition - line angle, point on line), sensor accuracy)
+    sensors = [((0.0, (0.0, 5.0)), 1.0),
+               ((math.pi / 4, (0.0, 0.0)), 1.0)]
 
-    print( accum_distSQ(sensors, (0, 0)) )
-
-
+    # find optimal point with minimizer
+    minFunc = lambda optPnt : sensor_errors_accum(sensors, optPnt)
+    optRes = opt.minimize(minFunc, [10, 10], options={'disp': False})
+    print("Optimal location: ", optRes.x)
 
