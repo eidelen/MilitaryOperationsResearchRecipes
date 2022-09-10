f7adb6a0892eca156898f7dc38bfb18b5bdf22d8
Adrian Schneider <adrian.schneider.pr@gmail.com>
2022-09-10 17:41:26 +0200

    add -dir

--- test_opResearch.py
+++ test_opResearch.py
@@ -6,10 +6,12 @@ import numpy as np
 class TestOperationsReseachRecipes(unittest.TestCase):
 
     def test_localization_distance(self):
-        # line: y = x
+        # line: y = x same for y = -x
         # test points on line -> zero dist
         for i in np.linspace(-10.0, 10.0, num=200):
             dist = localization.dist_pnt2line((math.pi / 4, (0.0, 0.0)), (i, i) )
+            self.assertAlmostEqual(dist, 0.0)
+            dist = localization.dist_pnt2line((math.pi / 4 + math.pi, (0.0, 0.0)), (i, i))
             self.assertAlmostEqual(dist, 0.0)
 
 
